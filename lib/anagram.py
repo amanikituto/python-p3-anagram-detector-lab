e44311781ac9b8a969b9037d42739ba538353dc1
amanikitutocd.. <akituto@gmail.com>
2024-05-20 01:05:11 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, candidates):
+        return [candidate for candidate in candidates if sorted(candidate) == self.sorted_word and candidate.lower() != self.word.lower()]
